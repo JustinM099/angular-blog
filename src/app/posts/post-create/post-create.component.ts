f684620d97ea66e4efe1f32bad499b71e8f6db7b
JustinM099 <justinm099@mac.com>
2022-07-15 22:29:17 -0700

    working on back end

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -23,7 +23,8 @@ export class PostCreateComponent implements OnInit {
   onAddPost(form: NgForm) {
     if (form.invalid) return;
 
-    this.postsService.addPosts(form.value.title, form.value.content)
+    this.postsService.addPost(form.value.title, form.value.content)
+    form.resetForm()
   }
 
 }
